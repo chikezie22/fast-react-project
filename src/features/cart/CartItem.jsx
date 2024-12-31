05af4b6ca0ec9bb1981b1bccb1825eee1aef2285
chikezie22 <simonchikezie44@gmail.com>
2024-12-31 16:52:12 +0100

    initialize React app with Vite, add basic components and routing

---
+++ src/features/cart/CartItem.jsx
@@ -0,0 +1,19 @@
+import { formatCurrency } from "../../utils/helpers";
+
+/* eslint-disable react/prop-types */
+function CartItem({ item }) {
+  const { name, quantity, totalPrice } = item;
+
+  return (
+    <li>
+      <p>
+        {quantity}&times; {name}
+      </p>
+      <div>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default CartItem;
