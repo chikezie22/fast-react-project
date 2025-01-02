951ac112531a349928abf450a06f7fc496343d72
chikezie22 <simonchikezie44@gmail.com>
2025-01-02 12:20:55 +0100

    add action to CreateOrder component and update routing; replace form with Form from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, { action as createOrderAction } from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
 import Cart from "./features/cart/Cart";
 import AppLayout from "./ui/AppLayout";
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order", element: <CreateOrder /> },
+      { path: "/order", element: <CreateOrder />, action: createOrderAction },
       { path: "/order/:orderId", element: <Order />, loader: orderLoader, errorElement: <Error /> },
     ],
   },
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 // import { useState } from "react";
 
+import { Form } from "react-router-dom";
+
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
@@ -37,7 +39,7 @@ function CreateOrder() {
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      <Form method="post">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -71,9 +73,15 @@ function CreateOrder() {
         <div>
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
 }
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data);
+};
 
 export default CreateOrder;
