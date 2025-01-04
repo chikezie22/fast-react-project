a1b875d42186f187815936cfd4fb7ffaaa222c38
chikezie22 <simonchikezie44@gmail.com>
2025-01-04 09:06:16 +0100

    refactor: update HTML structure and enhance styling across components

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 function Header() {
   return (
-    <div>
+    <header className="bg-yellow-500">
       <Link to={"/"}>Fast React Pizza</Link>
       <p>Simon</p>
       <SearchOrder />
-    </div>
+    </header>
   );
 }
 
