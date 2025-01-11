23bd7e335216c000fd4cc238799db88f0aa5e1bc
chikezie22 <simonchikezie44@gmail.com>
2025-01-12 00:20:24 +0100

    WD

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,15 +1,21 @@
-import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { totalPrice, totalQuantity } from './cartSlice'
+import { formatCurrency } from '../../utils/helpers'
 
 function CartOverview() {
-  return (
-    <div className="bg-stone-800 text-stone-300 uppercase p-4 sm:p-6 flex justify-between items-center">
-      <p className="font-semibold space-x-4 sm:space-x-6 text-sm sm:text-base">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
-      </p>
-      <Link to={"/cart"}>Open cart &rarr;</Link>
-    </div>
-  );
+    const totalPizzaPrice = useSelector(totalPrice)
+    const totalPizzaQuantity = useSelector(totalQuantity)
+
+    return (
+        <div className="bg-stone-800 text-stone-300 uppercase p-4 sm:p-6 flex justify-between items-center">
+            <p className="font-semibold space-x-4 sm:space-x-6 text-sm sm:text-base">
+                <span>{totalPizzaQuantity} pizzas</span>
+                <span>{formatCurrency(totalPizzaPrice)}</span>
+            </p>
+            <Link to={'/cart'}>Open cart &rarr;</Link>
+        </div>
+    )
 }
 
-export default CartOverview;
+export default CartOverview
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,36 +1,49 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
-    cart:[]
-};
+    cart: [],
+}
 const cartSlice = createSlice({
-    name:'cart',
+    name: 'cart',
     initialState,
-    reducers:{
-        addItem(state,action){
+    reducers: {
+        addItem(state, action) {
             // payload = newitem
             state.cart.push(action.payload)
         },
-        deleteItem(state,action){
+        deleteItem(state, action) {
             // payload = id of the item
-         state.cart = state.cart.filter(item=>item.id !== action.payload)
+            state.cart = state.cart.filter((item) => item.id !== action.payload)
         },
-        increaseItemQuantity(state,action){
-            const item = state.cart.find(item=>item.id=== action.payload);
-            item.quantity++;
-            item.totalPrice = item.quantity * item.unitPrice;
-        },
-        decreaseItemQuantity(state,action){
-            const item = state.cart.find(item=>item.id === action.payload);
-            item.quantity--;
+        increaseItemQuantity(state, action) {
+            const item = state.cart.find((item) => item.id === action.payload)
+            item.quantity++
             item.totalPrice = item.quantity * item.unitPrice
-            
         },
-        clearCart(state){
-        state.cart = initialState;
-        }
-    }
+        decreaseItemQuantity(state, action) {
+            const item = state.cart.find((item) => item.id === action.payload)
+            item.quantity--
+            item.totalPrice = item.quantity * item.unitPrice
+        },
+        clearCart(state) {
+            state.cart = initialState
+        },
+    },
 })
 
-export const {addItem,deleteItem, increaseItemQuantity,decreaseItemQuantity,clearCart} = cartSlice.actions;
+export const {
+    addItem,
+    deleteItem,
+    increaseItemQuantity,
+    decreaseItemQuantity,
+    clearCart,
+} = cartSlice.actions
 
-export default cartSlice.reducer;
+export default cartSlice.reducer
+
+export const totalPrice = (state) =>
+    state.cart.cart.reduce((acc, item) => {
+        return acc + item.totalPrice
+    }, 0)
+
+export const totalQuantity = (state) =>
+    state.cart.cart.reduce((acc, item) => acc + item.quantity, 0)
